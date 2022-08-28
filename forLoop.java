2f0c5dede2091fc21b14dbcf67c9735413205967
Summaiya Haider <haidersummaiya@gmail.com>
2022-08-28 21:18:10 +0530

    initial commit

--- forLoop.java
+++ forLoop.java
@@ -1,7 +1,7 @@
 public class forLoop {
     public static void main(String args[]) {
-        for(int i=0; i > 0; i++){
-            System.out.println(i);
+        for(int i=0; i < 10; i++){
+            System.out.println("Hello");
         }
     }
 }
